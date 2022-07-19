b17262e800a81b73d5b8ee89681a013e4bc4bce9
hot9cups <hot9cups@yahoo.in>
2022-07-19 20:00:41 +0530

    Adding Staff and Order Functionality

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,5 +1,6 @@
 import typing
 from dataclasses import dataclass
+from collections import defaultdict, deque
 
 
 @dataclass(frozen=True)
@@ -20,6 +21,19 @@ class RestaurantManager:
         already defined a staff dictionary.
         """
         self.staff = {}
+        self.speciality_to_ids = defaultdict(deque)
+
+    def find_staff_member(self, speciality: str):
+        """Return on-duty staff member with specified speciality.
+        This works in a Round-Robin fashion.
+
+        """
+        speciality_staff_members = self.speciality_to_ids[speciality]
+        while True:
+            member = speciality_staff_members.popleft()
+            if member in self.staff.keys():
+                speciality_staff_members.append(member)
+                return self.staff[member]
 
     async def __call__(self, request: Request):
         """Handle a request received.
@@ -31,4 +45,24 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+        json_data = request.scope
+        request_type = json_data["type"]
+
+        if request_type == "staff.onduty":
+            self.staff[json_data["id"]] = request
+            for item in json_data["speciality"]:
+                self.speciality_to_ids[item].append(json_data["id"])
+
+        elif request_type == "staff.offduty":
+            del self.staff[json_data["id"]]
+
+        elif request_type == "order":
+            speciality = json_data["speciality"]
+
+            staff_member = self.find_staff_member(speciality)
+
+            full_order = await request.receive()
+            await staff_member.send(full_order)
+
+            result = await staff_member.receive()
+            await request.send(result)
